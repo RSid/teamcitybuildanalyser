fb7d4b97d59de3237283a71a2d4a5f6359661e61
RSid <sm1th303@gmail.com>
2014-09-10 22:28:45 -0400

    change query function now shows name of person responsible

--- app/controllers/teamcity_controller.rb
+++ app/controllers/teamcity_controller.rb
@@ -1,10 +1,6 @@
 require 'teamcity'
 
 class TeamcityController < ApplicationController
-  def get_changes_helper(build_id)
-    TeamCity.get("changes?build=id:#{build_id}",
-      {user: TeamCity.http_user, password: TeamCity.http_password})
-  end
 
   # This only needs to be set once per Ruby execution.
   # You may use guestAuth instead of httpAuth and omit the use of http_user and http_password
@@ -16,7 +12,7 @@ class TeamcityController < ApplicationController
   end
 
   def index
-    @builds = TeamCity.builds
+    @builds = TeamCity.builds[0..5]
 
     @build_info = []
 
@@ -25,6 +21,27 @@ class TeamcityController < ApplicationController
     end
 
     @build_stats = TeamCity.build_statistics(@builds[0].id)
-    @changes = get_changes_helper(@builds[5].id)
+    @changes = get_changes_helper(@builds)
+  end
+
+  private
+
+  def get_changes_helper(builds)
+    all_builds_results = []
+    builds.each do |build|
+        build_results = TeamCity.get("changes?build=id:#{build.id}",
+          {user: TeamCity.http_user, password: TeamCity.http_password})
+        if build_results["count"] == 0
+          all_builds_results << [{build_id: build.id}, "No changes registered"]
+        else
+          build_changes = []
+          build_results["change"].each do |change|
+            raw_change_info = TeamCity.get("changes/id:#{change["id"]}")
+            build_changes << {user: raw_change_info.username, comments: raw_change_info.comment} #, changed_files: raw_change_info.files
+          end
+          all_builds_results << [build_id: build.id] + build_changes
+        end
+    end
+    return all_builds_results
   end
 end
